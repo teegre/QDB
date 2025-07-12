ec23c8cc94c11d76a06e1499e0be6a0a829c1e4f
Stéphane MEYER <tigerlost@me.com>
2025-07-12 10:50:22 +0200

    example database building script.

---
+++ build_database.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+echo "* Creating virtual environment..."
+python -m venv persons_venv
+echo "* Done."
+source persons_venv/bin/activate
+echo "* Virtual environment activated."
+echo "* Installing dependencies..."
+pip install faker &> /dev/null && {
+  echo "* Done."
+  echo "* Building database..."
+  python fixtures.py
+  echo "* Done."
+}
+deactivate
+rm -r persons_venv
